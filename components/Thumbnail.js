56859249aa9b0a670cd222563f1d12f88ca8286f
jmfigueroaGit <jheremiah.figueroa@tup.edu.ph>
2022-01-04 22:56:27 +0800

    updates

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -8,7 +8,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
 	return (
 		<div
 			ref={ref}
-			className="p-2 group cursor-pointer transition duration-100 ease-in transform sm:hover:scale-105 hover:z-50"
+			className="z-50 p-2 transition duration-100 ease-in transform cursor-pointer sm:hover:scale-105"
 		>
 			<Image
 				layout="responsive"
@@ -19,11 +19,11 @@ const Thumbnail = forwardRef(({ result }, ref) => {
 			/>
 
 			<div className="p-2">
-				<p className="truncate max-w-md">{result.overview}</p>
-				<h2 className="mt-1 text-2xl text-white transtion-all duration-100 ease-in-out group-hover:font-bold">
+				<p className="max-w-md truncate">{result.overview}</p>
+				<h2 className="mt-1 text-2xl text-white duration-100 ease-in-out transtion-all group-hover:font-bold">
 					{result.title || result.original_name}
 				</h2>
-				<p className="flex item-center capitalize opacity-0 group-hover:opacity-100 ">
+				<p className="flex capitalize opacity-0 item-center hover:opacity-100 ">
 					{result.media_type && `${result.media_type}`}{' '}
 					{result.release_date && `${result.release_date}`}{' '}
 					<ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
